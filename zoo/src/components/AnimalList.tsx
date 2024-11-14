f5ee82d0b925131947b53090203d23f34347f93e
Irimies Vasile <133998309+Vasile07@users.noreply.github.com>
2024-11-14 17:57:49 +0200

    finished project

--- zoo/src/components/AnimalList.tsx
+++ zoo/src/components/AnimalList.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonIcon, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonList, IonListHeader, IonLoading } from "@ionic/react";
+import { createAnimation, IonContent, IonIcon, IonImg, IonInfiniteScroll, IonInfiniteScrollContent, IonItem, IonLabel, IonList, IonListHeader, IonLoading } from "@ionic/react";
 import axios from "axios";
 import { act, useEffect, useReducer, useState } from "react";
 import { eye, newspaper } from 'ionicons/icons';
@@ -15,7 +15,8 @@ interface AnimalProperties {
     species: string,
     birthdate: string,
     isVaccinate: boolean,
-    weight: number
+    weight: number,
+    image: string,
 }
 
 interface ActionProperties {
@@ -23,6 +24,24 @@ interface ActionProperties {
     newAnimal?: AnimalProperties,
     animals?: AnimalProperties[]
 }
+
+function imageAnimation() {
+    const elements = document.querySelectorAll('.myImage'); // Select all elements with the .myImage class
+    elements.forEach((el) => {
+        const animation = createAnimation()
+            .addElement(el)
+            .duration(5000)
+            .direction('alternate')
+            .iterations(Infinity)
+            .keyframes([
+                { offset: 0, transform: 'rotate(0deg)', opacity: '1' },
+                { offset: 0.5, transform: 'rotate(180deg)', opacity: '1' },
+                { offset: 1, transform: 'rotate(360deg)', opacity: '1' }
+            ]);
+        animation.play();
+    });
+}
+
 
 function reducer(state: AnimalProperties[], action: ActionProperties): AnimalProperties[] {
     switch (action.type) {
@@ -55,22 +74,22 @@ const AnimalList: React.FC<ContainerProps> = () => {
     const [animals, dispatch] = useReducer(reducer, []);
     const [currentPage, setCurrentPage] = useState(1);
     const [hasMoreData, setHasMoreData] = useState(true);
-    
+
     const loadMore = async (event: any) => {
-        
+
         if (isLoading || !hasMoreData) {
             event.target.complete();
             return;
         }
-    
+
         setIsLoading(true);
-    
+
         try {
-        
+
             const res = await getAnimalsFromAPage(currentPage);
-    
+
             if (res.data.length > 0) {
-                dispatch({type: 'loading_more_entities', animals: res.data});
+                dispatch({ type: 'loading_more_entities', animals: res.data });
                 setCurrentPage(prevPage => prevPage + 1);
             } else {
                 setHasMoreData(false);
@@ -88,9 +107,9 @@ const AnimalList: React.FC<ContainerProps> = () => {
             setIsLoading(true);
             try {
                 const res = await getAnimalsFromAPage(page);
-    
+
                 if (res.data.length > 0) {
-                    dispatch({type: 'initialize', animals: res.data});
+                    dispatch({ type: 'initialize', animals: res.data });
                 } else {
                     setHasMoreData(false);
                 }
@@ -103,8 +122,12 @@ const AnimalList: React.FC<ContainerProps> = () => {
 
         fetchAnimals(currentPage);
         setCurrentPage(currentPage + 1);
-    },[])
-    
+    }, [])
+
+    useEffect(() => {
+        imageAnimation()
+    },[animals])
+
 
     useEffect(() => {
         const token = localStorage.getItem('token');
@@ -137,13 +160,21 @@ const AnimalList: React.FC<ContainerProps> = () => {
                         <IonItem key={animal.id}>
                             <IonIcon slot="start" icon={eye} color="#000" onClick={() => handleSeeDetails(animal.id)} />
                             <IonLabel>{animal.name}</IonLabel>
+                            <IonImg
+                                className="myImage"
+                                src={`data:image/jpeg;base64,${animal.image}`}
+                                slot="end"
+                                style={{ height: "80px", width: "auto", margin: "10px" }}
+                            />
                         </IonItem>
+
+
                     );
                 })}
             </IonList>
             <IonInfiniteScroll onIonInfinite={loadMore}>
-          <IonInfiniteScrollContent loadingSpinner="bubbles" loadingText="Loading more data..."></IonInfiniteScrollContent>
-        </IonInfiniteScroll>
+                <IonInfiniteScrollContent loadingSpinner="bubbles" loadingText="Loading more data..."></IonInfiniteScrollContent>
+            </IonInfiniteScroll>
         </IonContent>
     );
 }
--- zoo/src/pages/AnimalDetailsPage.tsx
+++ zoo/src/pages/AnimalDetailsPage.tsx
@@ -1,31 +1,100 @@
-import { IonCard, IonCardTitle, IonLabel, IonLoading, IonPage } from "@ionic/react";
+import { createAnimation, IonButton, IonCard, IonCardTitle, IonCheckbox, IonContent, IonFab, IonFabButton, IonHeader, IonIcon, IonImg, IonInput, IonItem, IonLabel, IonLoading, IonPage, IonTitle, IonToolbar } from "@ionic/react";
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { useParams } from "react-router";
 import "./AnimalDetailsPage.css"
 
+import { createOutline, saveOutline, cameraOutline } from 'ionicons/icons';
+import { useCamera } from "../hooks/useCamera";
+import { usePhotos } from "../hooks/usePhotos";
+import axiosInstance from "../common/axiosInstance";
+import { useFiles } from "../hooks/useFiles";
+import { useMyLocation } from "../hooks/useMyLocation";
+import MyMap from "../components/MyMap";
+import { Position } from "@capacitor/geolocation";
+
 interface ContainerProps { }
 
 interface AnimalProperties {
     id: number,
-    name : string 
-    species : string 
-    birthdate : string
-    isVaccinate : boolean
-    weight : number
+    name: string,
+    species: string,
+    birthdate: string,
+    isVaccinate: boolean,
+    weight: number,
+    image: string,
+    latitude: number,
+    longitude: number,
 }
+
+
+
+
+function imageAnimation() {
+    const el = document.querySelector('.myImage');
+    if (el) {
+        const animation = createAnimation()
+            .addElement(el)
+            .duration(3000)
+            .iterations(1)
+            .keyframes([
+                { offset: 0, transform: 'translateY(-100%)', opacity: '0' },
+                { offset: 1, transform: 'translateY(0)', opacity: '1' },
+            ]);
+        animation.play();
+    }
+}
+const customEnterAnimation = (baseEl: HTMLElement) => {
+    const backdropAnimation = createAnimation()
+        .addElement(baseEl.querySelector("ion-backdrop")!)
+        .fromTo("opacity", "0.01", "var(--backdrop-opacity)");
+
+    const wrapperAnimation = createAnimation()
+        .addElement(baseEl.querySelector(".loading-wrapper")!)
+        .keyframes([
+            { offset: 0, transform: "translateX(-100%) translateY(0)", opacity: "0" }, 
+            { offset: 0.3, transform: "translateX(30%) translateY(-50px)", opacity: "1" }, 
+            { offset: 0.6, transform: "translateX(10%) translateY(-20px)", opacity: "1" },
+            { offset: 1, transform: "translateX(0) translateY(0)", opacity: "1" }        
+        ]);
+
+    return createAnimation()
+        .addElement(baseEl)
+        .duration(800) // Total duration of the animation
+        .easing("ease-in-out") // Easing function for smooth effect
+        .addAnimation([backdropAnimation, wrapperAnimation]);
+};
 
 const AnimalDetailsPage: React.FC<ContainerProps> = () => {
 
+    
+    const myLocation = useMyLocation();
+    const [coords, setCoords] = useState<{latitude: number, longitude: number} | null>(null);
+
+    useEffect(() => {
+        if (myLocation.position?.coords) {
+          setCoords(myLocation.position.coords);
+        }
+      }, [myLocation.position]);
+
+    const { takePhoto } = usePhotos();
+
+    const { downloadImage } = useFiles();
+
     const { animalId } = useParams<{ animalId: string }>();
 
-    const [animal, setAnimal] = useState<AnimalProperties>();
+    const [animal, setAnimal] = useState<AnimalProperties | null>(null);
+
     const [isLoading, setIsLoading] = useState(true);
+
+    const [isEditable, setIsEditable] = useState(false);
 
     const fetchAnimalData = useCallback(() => {
         axios.get("http://localhost:3000/animals/" + animalId) // Corrected URL
             .then((res) => {
                 setAnimal(res.data);
+                if(res.data.latitude && res.data.longitude)
+                    setCoords({latitude: res.data.latitude, longitude: res.data.longitude})
                 console.log(res.data)
                 setIsLoading(false);
             })
@@ -39,30 +108,117 @@ const AnimalDetailsPage: React.FC<ContainerProps> = () => {
         fetchAnimalData();
     }, [fetchAnimalData]);
 
+    useEffect(() => {
+        if (animal) {
+            imageAnimation(); // Call the animation function when animal is loaded
+        }
+    }, [animal]);
+
+    const onLabelChanged = (label: keyof AnimalProperties, value: any) => {
+        setAnimal(prevAnimal => {
+            if (prevAnimal) {
+                return {
+                    ...prevAnimal,
+                    [label]: value,
+                };
+            }
+            return null; // or handle how you want to deal with null state
+        });
+    };
+
+    const handleEditSave = () => {
+        if (isEditable) {
+            axiosInstance.put(`http://localhost:3000/animals/update/${animal?.id}`,{...animal, latitude: coords?.latitude, longitude: coords?.longitude})
+            .then((response)=> {
+
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        }
+        setIsEditable(!isEditable);
+    }
+
+    const handleTakePhoto = () => {
+        takePhoto().then((data) => {
+            console.log(data);
+            onLabelChanged("image",data.base64String);
+        });
+    }
+
     return (
         <IonPage className="animal-page">
-            <IonLoading isOpen={isLoading} message="Searching for animal..." spinner="bubbles"/>
-            {animal != undefined && 
-            <IonCard className="animal-card">
-                <IonCardTitle>Animal: {animal.name}</IonCardTitle>
-                <IonLabel>Species: {animal.species}</IonLabel>
-                <div className="contentDiv">
-                    <div>
-                        <div>Weight</div>
-                        <div>{animal.weight}</div>
-                    </div>
-                    <div>
-                        <div>Age</div>
-                        <div>{animal.birthdate}</div>
-                    </div>
-                </div>
-                <div className="wild-caution-label">
-                    <IonLabel>VACCINATE: {animal.isVaccinate ? "✅" : "❌"}</IonLabel>
-                </div>
-            </IonCard>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Animal Page</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+            <IonLoading isOpen={isLoading} message="Searching for animal..." enterAnimation={customEnterAnimation} />
+            {animal &&
+
+                <IonContent>
+
+                    {animal.image && (
+                        <IonImg
+                            className="myImage"
+                            src={`data:image/jpeg;base64,${animal.image}`}
+                            alt="Captured"
+                            style={{
+                                width: '25%',
+                                height: 'auto',
+                                position: 'absolute', // Use absolute positioning if needed
+                                top: '0px',
+                                right: '0px',
+                            }}
+                            onClick={() => downloadImage(`data:image/jpeg;base64,${animal.image}`)}
+                        />
+                    )}
+
+                    {
+                       coords && (
+                        <MyMap 
+                            isEditable={isEditable}
+                          lat={coords.latitude}
+                          lng={coords.longitude}
+                          onMapClick={(e) => {
+                            console.log("Map clicked at:", e);
+                            if(isEditable)
+                                setCoords({ latitude: e.latitude, longitude: e.longitude });
+                          }}
+                          onMarkerClick={(e) => {
+                            
+                            console.log("Marker clicked at:", e);
+                            if(isEditable)
+                                setCoords({ latitude: e.latitude, longitude: e.longitude });
+                          }}
+                        />
+                      )
+                    }
+                    <IonInput disabled={!isEditable} style={{ marginTop: "20px" }} label="Name" value={animal.name} onIonChange={(e: any) => onLabelChanged("name", e.detail.value)} labelPlacement="floating" fill="outline" placeholder="Enter text" className="input-field"></IonInput>
+                    <IonInput disabled={!isEditable} style={{ marginTop: "20px" }} label="Species" value={animal.species} onIonChange={(e: any) => onLabelChanged("species", e.detail.value)} labelPlacement="floating" fill="outline" placeholder="Enter text" className="input-field"></IonInput>
+                    <IonInput disabled={!isEditable} style={{ marginTop: "20px" }} label="Birthdate" value={animal.birthdate.split('T')[0]} onIonChange={(e: any) => onLabelChanged("birthdate", e.detail.value)} labelPlacement="floating" fill="outline" placeholder="Enter text" className="input-field"></IonInput>
+                    <IonCheckbox disabled={!isEditable} color="dark" style={{ marginTop: "20px", marginLeft: "50px", color: "#000" }} value={animal.isVaccinate} onIonChange={(e: any) => onLabelChanged("isVaccinate", e.detail.value)} labelPlacement="start">Is Vaccinate</IonCheckbox>
+                    <IonInput disabled={!isEditable} style={{ marginTop: "20px" }} label="Weight" value={animal.weight} onIonChange={(e: any) => onLabelChanged("weight", e.detail.value)} labelPlacement="floating" fill="outline" placeholder="Enter text" className="input-field"></IonInput>
+                </IonContent>
             }
-        </IonPage>    
+
+            <IonFab vertical="bottom" horizontal="end">
+                <IonFabButton onClick={handleEditSave}>
+                    <IonIcon icon={isEditable ? saveOutline : createOutline} />
+                </IonFabButton>
+            </IonFab>
+            {
+                isEditable &&
+                <IonFab vertical="bottom" horizontal="start">
+                    <IonFabButton onClick={handleTakePhoto} >
+                        <IonIcon icon={cameraOutline} />
+                    </IonFabButton>
+                </IonFab>
+            }
+
+        </IonPage>
     );
+
 }
 
 export default AnimalDetailsPage;
